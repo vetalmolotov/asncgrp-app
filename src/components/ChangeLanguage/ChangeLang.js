3d2d4a755ccb8fae1dd00ea4f5c5de8efaee35bd
vetalmolotov <vetal.molotov@gmail.com>
2023-08-28 16:01:59 +0300

    display language flags using map() function

--- src/components/ChangeLanguage/ChangeLang.js
+++ src/components/ChangeLanguage/ChangeLang.js
@@ -5,21 +5,32 @@ import '../../assets/styles/header/change-lang.scss'
 
 const ChangeLang = () => {
 
+    const langFlagItem = [
+        {
+            id: '0',
+            icon: <LangGerman/>
+        },
+        {
+            id: '1',
+            icon: <LangTurkish/>
+        },
+        {
+            id: '2',
+            icon: <LangUkraine/>
+        }
+    ]
+
     return <>
         <div className={`user-bar-lang ${lang.langWrapper} active`}>
             <div className={lang.langCurrent}>
                 <LangEng/>
             </div>
             <ul className={lang.langList}>
-                <li>
-                    <LangGerman/>
-                </li>
-                <li>
-                    <LangTurkish/>
-                </li>
-                <li>
-                    <LangUkraine/>
-                </li>
+                {langFlagItem.map((option) => (
+                    <li key={option.id}>
+                        {option.icon}
+                    </li>
+                ))}
             </ul>
         </div>
     </>
